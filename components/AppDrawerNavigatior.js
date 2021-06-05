51b6a8a7f183cce94a6ab7bd64fc3d2d0dfc16d2
Taimoor23 <73740673+Taimoor23@users.noreply.github.com>
2021-06-05 09:37:02 +0100

    Update AppDrawerNavigatior.js

--- components/AppDrawerNavigatior.js
+++ components/AppDrawerNavigatior.js
@@ -2,7 +2,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import { AppTabNavigator } from './AppTabNavigator'
 import CustomSidebarMenu  from './CustomSidebarMenu';
 import MyBartersScreen from '../screens/MyBartersScreen';
-import SettingScreen from '../screens/SettingScreen';
+import SettingScreen from '../components/SettingScreen';
 import NotificationScreen from '../screens/NotificationScreen';
 
 export const AppDrawerNavigator = createDrawerNavigator({
